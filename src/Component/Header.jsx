6354ce5ecfa35e825fd2727fc021ead9bd789764
Akinmolayan john <johnayomide50@gmail.com>
2023-12-11 17:26:30 -0800

    updated desktop view nav bar

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
              <h2 className='text-white hover:underline'>SERIES</h2>
          </div>
          <div>
-            <img src='https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=740&t=st=1702343609~exp=1702344209~hmac=35ba3b139f94212d9dfe5ce11a5014167edda673f480b84df352dec65d603755' className='rounded-full w-[5    0px] items-end' />
+            <img src='https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=740&t=st=1702343609~exp=1702344209~hmac=35ba3b139f94212d9dfe5ce11a5014167edda673f480b84df352dec65d603755' className='rounded-full w-[50px] items-end' />
          </div>
       </div>
     </div>
