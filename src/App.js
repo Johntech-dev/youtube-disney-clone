b49f16551b91204bb94f3b4d6f032e355f964191
Akinmolayan john <johnayomide50@gmail.com>
2023-12-15 16:52:29 -0800

    Finished the project

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.css'
 import Header from './Component/Header';
 import ProductionHouse from './Component/ProductionHouse';
 import Slider from './Component/Slider';
+import GenreMovieList from './Component/GenreMovieList';
 
 function App() {
-  const containerStyle = {
-    backgroundImage: "url('home-background.png')",
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '100vh',
-  };
+
+  // const containerStyle = {
+  //   backgroundImage: "url('home-background.png')",
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center',
+  //   height: '100vh',
+  // };
   return (
-    <div style={containerStyle}>
+    <div>
       <Header />
+
       <Slider />
+
       <ProductionHouse />
+
+      <GenreMovieList/>
      </div>
   );
 }
--- src/Component/Slider.jsx
+++ src/Component/Slider.jsx
@@ -32,8 +32,7 @@ function Slider() {
         onClick={()=>sliderRight(elementRef.current)} />
 
    
-    <div className='flex overflow-x-auto w-full px-16 py-4
-    scrollbar-none scroll-none' ref={elementRef}> 
+    <div className='flex overflow-x-auto w-full px-16 py-4 ' ref={elementRef}> 
         {movieList.map((item)=>(
             <img src={IMAGE_BASE_URL+item.backdrop_path} 
             className='min-w-full  md:h-[310px] object-cover
